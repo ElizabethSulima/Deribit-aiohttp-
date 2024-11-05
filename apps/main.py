fd81b548e6ded4d8e280631580a503bffdce0361
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2024-11-05 19:01:13 +0500

    lifespan

--- apps/main.py
+++ apps/main.py
@@ -1,9 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from settings import config
 
 from api import image_router, tags_router, user_router
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    config.MEDIA_DIR.mkdir(exist_ok=True)
+    application.mount(
+        "/media", StaticFiles(directory=config.MEDIA_DIR), name="media"
+    )
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(image_router)
 app.include_router(user_router)
